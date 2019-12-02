2ee2109d0dccfc6596b325f5c4a89021e4f37862
Harshbir Singh <harshbhullar011@gmail.com>
2019-12-03 09:36:08 +1300

    Add project files.

---
+++ googleMap/MainActivity.cs
@@ -0,0 +1,95 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using Android.Locations;
+using System.Collections.Generic;
+using Android.Util;
+using System.Linq;
+using System;
+using Xamarin.Essentials;
+
+namespace googleMap
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnMap;
+        Button btnLocation;
+        string ans = "";
+        
+       
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+          
+            btnMap = FindViewById<Button>(Resource.Id.btnText);
+           //open  the google map app using 
+            btnMap.Click += delegate {
+                var geoUri = Android.Net.Uri.Parse("geo:42.37426660,-71.12420824");
+                var mapIntent = new Intent(Intent.ActionView, geoUri);
+                StartActivity(mapIntent);
+            };
+
+            btnLocation = FindViewById<Button>(Resource.Id.btnLocation);
+            btnLocation.Click += async delegate
+            {
+               /* ans = "Chennai International airport";
+                try
+                {
+                    var lat = 42.37426660;
+                    var lon = -71.12420824;
+
+                    var placemarks = await Geocoding.GetPlacemarksAsync(lat, lon);
+
+                    var placemark = placemarks?.FirstOrDefault();
+                    if (placemark != null)
+                    {
+                        var geocodeAddress =
+                            $"AdminArea:       {placemark.AdminArea}\n" +
+                            $"CountryCode:     {placemark.CountryCode}\n" +
+                            $"CountryName:     {placemark.CountryName}\n" +
+                            $"FeatureName:     {placemark.FeatureName}\n" +
+                            $"Locality:        {placemark.Locality}\n" +
+                            $"PostalCode:      {placemark.PostalCode}\n" +
+                            $"SubAdminArea:    {placemark.SubAdminArea}\n" +
+                            $"SubLocality:     {placemark.SubLocality}\n" +
+                            $"SubThoroughfare: {placemark.SubThoroughfare}\n" +
+                            $"Thoroughfare:    {placemark.Thoroughfare}\n";
+
+                        Console.WriteLine(geocodeAddress);
+                    }
+                }
+                catch (FeatureNotSupportedException fnsEx)
+                {
+                    // Feature not supported on device
+                }
+                catch (Exception ex)
+                {
+                    // Handle exception that may have occurred in geocoding
+                }
+
+    */
+
+
+            };
+        }
+        
+       
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+       
+
+    }
+}
